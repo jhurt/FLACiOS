6a3ed4c61b3bd0f331e348fa360a01ab66f4c0ad
Jason Hurt <jleehurt@gmail.com>
2012-07-30 11:55:17 -0700

    initial commit

---
+++ FLACiOS/FLACiOS.h
@@ -0,0 +1,23 @@
+//
+//  FLACiOS.h
+//  FLACiOS
+//
+//  Created by Jason Hurt on 7/30/12.
+//  Copyright (c) 2012 UNLV. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#include <FLAC/all.h>
+#include <FLAC/assert.h>
+#include <FLAC/callback.h>
+#include <FLAC/export.h>
+#include <FLAC/format.h>
+#include <FLAC/metadata.h>
+#include <FLAC/ordinals.h>
+#include <FLAC/stream_encoder.h>
+#include <FLAC/stream_decoder.h>
+
+@interface FLACiOS : NSObject
+
+@end
---
+++ FLACiOS/FLACiOS.m
@@ -0,0 +1,13 @@
+//
+//  FLACiOS.m
+//  FLACiOS
+//
+//  Created by Jason Hurt on 7/30/12.
+//  Copyright (c) 2012 UNLV. All rights reserved.
+//
+
+#import "FLACiOS.h"
+
+@implementation FLACiOS
+
+@end
